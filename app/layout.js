6dc87a05003ab4da803f000af7f781d418562e3a
MohanapriyaML <mohanapriyamyilsamy@genpixels.com>
2024-09-04 10:13:24 +0530

    meta correction

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,6 @@ import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Portfolio of Santhoshkumar - Software Developer",
-  description:
-    "This is the portfolio of Santhoshkumar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
