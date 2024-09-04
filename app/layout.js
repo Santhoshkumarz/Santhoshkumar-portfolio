1bbd1785ca6fd7c9a73c3cd8c0967619c4dd0fb7
MohanapriyaML <mohanapriyamyilsamy@genpixels.com>
2024-09-04 13:51:40 +0530

    xml file-include

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="title" content="Santhoshkumar Portfolio"></meta>
+        <title>Santhoshkumar Portfolio</title>
         <meta
           name="description"
           content="Hi, I&#039;m Santhosh Kumar. I’m a passionate and enthusiastic software developer with over a year of experience at Genpixels Tech Pvt Ltd in Coimbatore.&lt;br /&gt;&lt;br /&gt;    I’m a quick learner who thrives on tackling new challenges and solving problems..."
