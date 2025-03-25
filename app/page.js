0899787c110a1140e543be1bed171f055a7b9ba8
Santhoshkumarz <id.santhoshk@gmail.com>
2025-03-25 13:10:22 +0530

    fi

--- app/page.js
+++ app/page.js
@@ -1,24 +1,52 @@
-// pages/index.js
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import AboutSection from './components/homepage/about'
-import Blog from './components/homepage/blog';
-import ContactSection from './components/homepage/contact';
-import Education from './components/homepage/education';
-import Experience from './components/homepage/experience';
-import HeroSection from './components/homepage/hero-section';
-import Projects from './components/homepage/projects';
-import Skills from './components/homepage/skills';
-import { certificateData } from '../utils/data/certificates'; // Adjust the path as needed
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import components that rely on client-side features
+const HeroSection = dynamic(
+  () => import("./components/homepage/hero-section"),
+  { ssr: false }
+);
+const AboutSection = dynamic(() => import("./components/homepage/about"), {
+  ssr: false,
+});
+const Blog = dynamic(() => import("./components/homepage/blog"), {
+  ssr: false,
+});
+const ContactSection = dynamic(() => import("./components/homepage/contact"), {
+  ssr: false,
+});
+const Education = dynamic(() => import("./components/homepage/education"), {
+  ssr: false,
+});
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
+const Projects = dynamic(() => import("./components/homepage/projects"), {
+  ssr: false,
+});
+const Skills = dynamic(() => import("./components/homepage/skills"), {
+  ssr: false,
+});
+
+import { certificateData } from "../utils/data/certificates"; // Adjust the path as needed
 
 export default function Home() {
   const [certificates, setCertificates] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Simulate a data fetch
-    setCertificates(certificateData);
+    setTimeout(() => {
+      setCertificates(certificateData);
+      setLoading(false);
+    }, 1000); // Simulating a delay for demonstration
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
